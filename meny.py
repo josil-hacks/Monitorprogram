f43ae2bcc690c4a50c9a9fa21d47df5a39dd7837
John Silvell <john.silvell@josil.se>
2024-10-01 09:55:47 +0200

    Lade till en ny fil

---
+++ meny.py
@@ -0,0 +1,13 @@
+# Function to show menu
+def show_menu():
+    print()
+    print("=============================")
+    print()
+    print("1. Starta övervakning")
+    print("2. Lista aktiv övervakning")
+    print("3. Skapa larm")
+    print("4. Visa larm")
+    print("5. Starta övervakningsläge")
+    print("6. Stäng programmet")
+    print()
+    print()
